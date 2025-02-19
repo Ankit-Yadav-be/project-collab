acb1b0a3e30416e2a2302afd384156606a9f996b
Ankit yadav <ay870421@gmail.com>
2025-02-19 21:53:57 +0530

    app on Production

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,10 @@ import Useroute from "./route/userRoutes.js";
 import Projectroute from "./route/projectRoutes.js";
 import aiRoutes from "./route/aiRoutes.js";
 import cors from "cors";
+import path from "path";
 const app = express();
 connect();
+const _dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +16,10 @@ app.use("/api/v1/user", Useroute);
 app.use("/api/v1/project",Projectroute);
 app.use("/api/v1/ai",aiRoutes);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 export default app;
 
