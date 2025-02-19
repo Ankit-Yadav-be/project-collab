562097f59780d5407f9f33b0e5a21ea000422dde
Ankit yadav <ay870421@gmail.com>
2025-02-19 23:22:05 +0530

    Fix: Removed backend dependency & added Node.js versions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@chakra-ui/react": "/node_modules/@chakra-ui/react",
+      "@chakra-ui/react": path.resolve(__dirname, "node_modules/@chakra-ui/react"),
     },
   },
 });
